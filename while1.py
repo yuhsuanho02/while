3b13992de7b1d0d621c9e6bd74df06565805cef5
Adam Ho <yuhsuanho02@gmail.com>
2022-01-09 14:25:38 +0800

    firstcommit

---
+++ while1.py
@@ -0,0 +1,9 @@
+while True:
+    mode = input("請輸入遊戲模式:")
+    if mode == 'q':#quit
+        break
+    if mode == '1':
+        print('啟動模式一')
+    if mode == '2':
+        print('啟動模式二')
+        
