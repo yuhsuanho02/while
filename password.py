b6e47c9fc24e4bc7b0b931265120734e97e0f1ed
Adam Ho <yuhsuanho02@gmail.com>
2022-01-09 16:12:48 +0800

    password finished

--- password.py
+++ password.py
@@ -1,10 +1,14 @@
 password = 'a1234567'
 i=3
 while i > 0:
+    i = i - 1
     pwd = input('請輸入你的密碼: ')
     if pwd == password:
         print('成功登入')
         break
     else:
-        i = i - 1
-        print('密碼錯誤!還有', i, '次機會')
+        print('密碼錯誤!')
+        if i > 0:
+            print('還有', i, '次機會')
+        else:
+            print('帳戶已鎖定!請重新更該密碼!')
